708550b7ad7d9c6192359396db84cedf8de8f4da
faithleo8 <leofaith8@gmail.com>
2023-05-19 21:22:36 +0300

    freeing paramaters

--- param_free.c
+++ param_free.c
@@ -11,17 +11,20 @@
  */
 void free_params(param_t *params)
 {
-unsigned int i;
+unsigned int x;  /* Loop variable */
 
-if (params->buffer)
-free(params->buffer);
-if (params->nextCommand)
-free(params->nextCommand);
-free_list(params->env_head);
-free_list(params->alias_head);
+if (params->buffer)  /* Check if buffer is not NULL */
+free(params->buffer);  /* Free buffer memory */
 
-for (i = 0; params->args[i]; i++)
-free(params->args[i]);
-free(params->args);
-free(params);
+if (params->nextCommand)  /* Check if nextCommand is not NULL */
+free(params->nextCommand);  /* Free nextCommand memory */
+
+free_list(params->env_head);  /* Free env_head list memory */
+free_list(params->alias_head);  /* Free alias_head list memory */
+
+for (x = 0; params->args[x]; x++)  /* Loop through args array */
+free(params->args[x]);  /* Free each argument memory */
+
+free(params->args);  /* Free args array memory */
+free(params);  /* Free params structure memory */	}
 }
