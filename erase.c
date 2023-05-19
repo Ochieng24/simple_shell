981da1c9f2f90fb8ed3f88410f0a0c6974499942
faithleo8 <leofaith8@gmail.com>
2023-05-19 14:43:50 +0300

    clear screen and move cursor

--- erase.c
+++ erase.c
@@ -2,13 +2,12 @@
 #include "shell.h"
 
 /**
- * _clear - clears the terminal screen
- * @params: parameters
+ * _clear - erases the shell screen
+ * @params: parameters passed
  *
  * Return: void
  */
 void _clear(param_t *params)
 {
-(void)params;
-_printf("\033[2J\033[1;1H");
-}
+(void)params;/*suppress compiler warning*/
+_printf("\033[2J\033[1;1H");/*prints ANSI escape char to clear & move cursor*/
