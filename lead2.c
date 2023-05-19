383929db82f12846f9709c542decf125d2008fa4
faithleo8 <leofaith8@gmail.com>
2023-05-19 20:27:08 +0300

    finding path function

--- lead2.c
+++ lead2.c
@@ -11,24 +11,30 @@
  */
 char *get_file(param_t *params)/*Retrieve the proper path to a command file*/
 {
-char *execute_path = NULL, *execute_args = NULL, *tmp = NULL, *current_state = NULL, *path = NULL;
+char *execute_path = NULL, *execute_args = NULL, *tmp = NULL;
+char *current_state = NULL, *path = NULL;
+/*check if command file exists and is executable*/
 if (access(params->args[0], F_OK | X_OK) == 0)
 {
 free(path);
 return (strdup(params->args[0]));
 }
+/*check for permissions denied error*/
 if (errno == EACCES)
 {
 params->status = 126;
 write_error(params, "Permission denied\n");
 return (NULL);
 }
+/*Get PATH environment variable*/
 path = _getenv("PATH", params);
+/*check if PATH is not set*/
 if (!path)
 {
 write_error(params, "not found\n");
 return (NULL);
 }
+/*tokenize path variable and search for command file*/
 execute_path = _strtok(path, ":", &current_state);
 while (execute_path)
 {
@@ -38,6 +44,7 @@ free(tmp);
 tmp = execute_args;
 execute_args = str_concat(execute_args, params->args[0]);
 free(tmp);
+/*checks if the command file exists*/
 if (access(execute_args, F_OK) == 0)
 {
 free(path);
@@ -47,6 +54,7 @@ return (execute_args);
 free(execute_path);
 execute_path = _strtok(NULL, ":", &current_state);
 }
+/*prints the error if file not found*/
 params->status = 127;
 write_error(params, "not found\n");
 free(path);
