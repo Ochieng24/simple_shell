742dc404c78d36a64b40accd9030613867d5ab13
ochieng24 <edwine.ochi24@gmail.com>
2023-05-19 08:30:23 -0700

    exiting.c

--- exiting.c
+++ exiting.c
@@ -10,13 +10,13 @@
  */
 int validNum(char *s)
 {
-	while (*s)
+	while (*s)/*loop thr char*/
 	{
-		if (*s > '9' || *s < '0')
-			return (0);
-		s++;
+		if (*s > '9' || *s < '0')/*if char ! a digit*/
+			return (0);/*Return 0 if char !a digit*/
+		s++;/*to next char*/
 	}
-	return (1);
+	return (1);/*Return 1 all char digits*/
 }
 /**
  * _myExit - buildin exit function
@@ -26,33 +26,32 @@ void _myExit(param_t *params)
 {
 	int status = 0;
 
-	if (!params->args[1])
+	if (!params->args[1])/*no argument after command*/
 	{
-		status = params->status;
-		free_params(params);
-		exit(status);
+		status = params->status;/* current status*/
+		free_params(params);/* set mem free*/
+		exit(status);/* end with current status*/
 	}
-	if (validNum(params->args[1]))
+	if (validNum(params->args[1]))/*argument valid nmbr*/
 	{
-		status = _atoi((params->args)[1]);
+		status = _atoi((params->args)[1]);/* convert to integer*/
 		if (status == -1)
 		{
-			write_error(params, "Illegal number: ");
-			write(STDERR_FILENO, params->args[1],
-			      _strlen(params->args[1]));
-			write(STDERR_FILENO, "\n", 1);
-			params->status = 2;
-			return;
+			write_error(params, "Illegal number: ");/* error msg to strd output*/
+			write(STDERR_FILENO, params->args[1],/*newline to strd output*/
+			      _strlen(params->args[1]));/*argument to  strd error output*/
+			write(STDERR_FILENO, "\n", 1);/*new line char to strd output*/ 
+			params->status = 2;/*set 2*/
+			return;/*Return*/
 		}
-		free_params(params);
-		exit(status);
+		free_params(params);/*mem allctd for prmtr*/
+		exit(status);/*end prog wth spcfd status*/
 	}
 	else
 	{
-		params->status = 2;
+		params->status = 2;/*set 2*/
 		write_error(params, "Illegal number: ");
-		write(STDERR_FILENO, params->args[1],
-		      _strlen(params->args[1]));
+		write(STDERR_FILENO, params->args[1], _strlen(params->args[1]));
 		write(STDERR_FILENO, "\n", 1);
 	}
 }
