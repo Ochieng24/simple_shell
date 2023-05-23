99a0a848be9d729b06f7f46108eeae7eb34c7561
faithleo8 <leofaith8@gmail.com>
2023-05-23 21:34:41 +0300

    retriving files

--- retrieve_file1.c
+++ retrieve_file1.c
@@ -2,101 +2,101 @@
 #include <stdlib.h>
 
 /**
- *  * rev_string - Reverses the order of characters in a string.
- * @s: A pointer to a null-terminated string to be reversed.
+ * rev_string - puts characters in a string in reverse order.
+ * @s: a pointer to the reversed version of a null-terminated string.
  */
 void rev_string(char *s)
 {
-int head, tail;
-int len;
-char tmp;
+int h, t;
+int l;
+char temp;
 if (s == NULL)
 {
 s = "(null)";
 }
-for (len = 0; s[len] != '\0'; len++)
+for (l = 0; s[l] != '\0'; l++)
 ;
-head = 0;
-tail = len - 1;
-while (head < tail)
+h = 0;
+t = l - 1;
+while (h < t)
 {
-tmp = *(s + head);
-*(s + head) = *(s + tail);
-*(s + tail) = tmp;
-head++;
-tail--;
+temp = *(s + h);
+*(s + h) = *(s + t);
+*(s + t) = temp;
+h++;
+t--;
 }
 }
 /**
 *get_rev - returns a reversed copy of a string
-* @s: string to be reversed
-*Return: string reversed
+*@s: returns a string's mirrored copy.
+*Return: reversed str
 */
 char *get_rev(char *s)
 {
-char *ptr = NULL;
+char *points = NULL;/*declare a * var & initialise it to null*/
 
-ptr = get_string(s);
-if (!ptr)
+points = get_string(s);
+if (!points)/*check if points is null*/
 {
 return (NULL);
 }
 if (s)
 {
-rev_string(ptr);
+rev_string(points);/*call the rev_string func as an arg*/
 }
-return (ptr);
+return (points);/*return the revesed str points*/
 }
 
 /**
- * rot13 - performs rot13 encryption on a string
- * @s: string to be encrypted
+ * rot13 - encrypts a string
+ * @s: str to be encrypted
  *
- * Return: pointer to the encrypted string
+ * Return: * to encrypted str
  */
 char *rot13(char *s)
 {
-char *ptr;
-int i;
-char c[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+char *points;/*declare a * var*/
+int x;
+char charac[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-char r[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+char ray[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-for (ptr = s; *ptr != '\0'; ptr++)
+for (points = s; *points != '\0'; points++)/*iterate thru each char in str*/
 {
-for (i = 0; i < 52; i++)
+for (x = 0; x < 52; x++)/*iterate thru each char in array*/
 {
-if (*ptr == c[i])
+if (*points == charac[x])/*check if teh current char matches char in array*/
 {
-*ptr = r[i];
+*points = ray[x];/*rep teh char with the corresponding array char*/
 break;
 }
 }
 }
-return (s);
+return (s);/*return modified str*/
 }
 
 /**
  * get_rot13 - get rot13
- * @s: string
- * Return: ptr to string
+ * @s: str
+ * Return: * to str
  */
 char *get_rot13(char *s)
 {
-char *ptr = NULL;
+char *points = NULL;
 
-ptr = get_string(s);
-if (!ptr)
+points = get_string(s);/*declare a pointer var and initialise*/
+if (!points)
 return (NULL);
 if (s)
 {
-rot13(ptr);
+rot13(points);/*call the rot13 function as arg*/
 }
-return (ptr);
+return (points);
 }
